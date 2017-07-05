dcc75999ceec587f4d93bd935871e65a1ee99121
ShenJingBingA <532510674@qq.com>
2017-07-06 00:02:38 +0800

    cll

---
+++ static/js/cll-ar.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2017/7/5.
+ */
+$(function () {
+    var getCss = function(o,key){
+        return o.currentStyle? o.currentStyle[key] : document.defaultView.getComputedStyle(o,false)[key];
+    };
+
+    function drag(element){
+        element.style.top = getCss(element,"top");
+        element.style.left = getCss(element,"left");
+        var hammer = new Hammer(element);//hammer实例
+        var x = 0;
+        var y = 0;
+        hammer.on('panstart',function(event){
+            x = parseInt(element.style.left);
+            y = parseInt(element.style.top);
+        });
+        hammer.on('panmove',function(event){
+            element.style.top = y + event.deltaY + "px";
+            element.style.left = x + event.deltaX + "px";
+        });
+        hammer.on('panend',function(event){
+
+        });
+
+    }
+    var myElement = document.getElementById("move-box");
+    drag(myElement);
+})
---
+++ static/js/cll-classify.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2017/7/5.
+ */
+$(function () {
+    var fashion=$(".fashion");
+    var con=$(".fashion .con");
+    var i=0;
+    var k;
+    var cona=$(".fashion .con a");
+
+    touch.on(fashion,"dragstart",con,function (e) {
+        if(e.direction=="left"){
+            i++;
+            k=5;
+        }else if(e.direction=="right"){
+            i--;
+            k=5
+        }
+
+        if(i>=0&&i<5){
+            for(var j=i;j<cona.length;j++){
+                cona.eq(j).css("z-index",k)
+                k--;
+            }
+        }
+    });
+})
