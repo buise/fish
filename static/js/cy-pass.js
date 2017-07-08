e71c898eb20ffc0d1e72ab33d6a712c4426348c2
QianRuoxi <914274917@qq.com>
2017-07-08 09:44:56 +0800

    'cy'

--- static/js/cy-pass.js
+++ static/js/cy-pass.js
@@ -1,26 +1,78 @@
-$().ready(function(){
-    var aa=$("input[type='password']");
+$(function () {
+    var pay_con = document.querySelector('.pay_con');
+    var activeIndex = 0;
+    var passWord = [];
+    toNext(0);
+    pay_con.addEventListener('keypress',function(e) {
+        if (e.target.className.toLowerCase() == 'pay_input') {
+            e.preventDefault();
+            if (activeIndex < 6 && e.keyCode != 8) {
+                var k = String.fromCharCode(e.charCode);
+                if (/\d/.test(k)) {
+                    $("#"+e.target.id).val(1);
+                    passWord.push(k);
+                    if (activeIndex != 6 - 1) {
+                        toNext(activeIndex + 1);
+                    }
+                    activeIndex++;
+                } else {
+                }
+            }
+            if (activeIndex == 6) {
+                if(passWord[5].value!=""){
+                    // console.log(document.querySelector(".button"))
+                    document.querySelector(".button").href="cy-red.html"
+                }
 
-    for(var i = 0; i<aa.length;i++){
-        var t = aa[i];
-        t.index = i;
-        t.setAttribute("readonly", true);
-        t.onkeyup=function(){
-            this.value=this.value.replace(/^(.).*$/,'$1');
-            var next = this.index + 1;
-            if(next > aa.length - 1) return;
-            aa[next].removeAttribute("readonly");
-            aa[next].focus();
+            }
         }
+    },false);
+    pay_con.addEventListener('keyup',function(e) {
+        var e = e || window.event;
+        if (e.keyCode == 8) {
+            if (activeIndex > 0) {
+                activeIndex--;
+                reback(activeIndex);
+                passWord.pop();
+            }
+        }
+    },false);
+    function toNext(index) {
+        var key = index + 1;
+        $("#pay_input_" + key).focus();
     }
+    function reback(index) {
+        var key = index + 1;
+        $("#pay_input_" + key).val('');
+        $("#pay_input_" + key).focus();
+    }
+
+    var aa=$("input[type='password']");
+    aa.each(function (index,val) {
+        val.setAttribute("readonly", "readonly");
+
+    })
     aa[0].removeAttribute("readonly");
 
 
-    $("[type='password']").each(function (index,val) {
-        $(val).change(function () {
-            if ( $(val).val()!=""){
-                document.querySelector(".button").href="cy-red.html"
-            }
-        })
-    })
+
 })
+
+
+// $().ready(function(){
+//     var aa=$("input[type='password']");
+//
+//     for(var i = 0; i<aa.length;i++){
+//         var t = aa[i];
+//         t.index = i;
+//         t.setAttribute("readonly", true);
+//         t.onkeyup=function(){
+//             this.value=this.value.replace(/^(.).*$/,'$1');
+//             var next = this.index + 1;
+//             if(next > aa.length - 1) return;
+//             aa[next].removeAttribute("readonly");
+//             aa[next].focus();
+//         }
+//     }
+//     aa[0].removeAttribute("readonly");
+// })
