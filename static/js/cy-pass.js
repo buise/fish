4e8a7e94cd9ebc8839cf1a536ae76e601e53cdd8
QianRuoxi <914274917@qq.com>
2017-07-09 09:47:58 +0800

    cy

--- static/js/cy-pass.js
+++ static/js/cy-pass.js
@@ -1,156 +1,91 @@
-<<<<<<< HEAD
-/**
- * Created by Administrator on 2017/7/3.
- */
-// window.onload=function(){
-//     var pay_con = document.querySelector('.pay_con');
-//     var activeIndex = 0;
-//     var passWord = [];
-//     toNext(0);
-//     pay_con.addEventListener('touchstart',function(e) {
-//         if (e.target.className.toLowerCase() == 'pay_input') {
-//             e.preventDefault();
-//             if (activeIndex < 6 && e.keyCode != 8) {
-//                 var k = String.fromCharCode(e.charCode);
-//                 if (/\d/.test(k)) {
-//                     $("#"+e.target.id).val(1);
-//                     passWord.push(k);
-//                     if (activeIndex != 6 - 1) {
-//                         toNext(activeIndex + 1);
-//                     }
-//                     activeIndex++;
-//                 } else {
-//                 }
-//             }
-//             if (activeIndex == 6) {
-//                 alert(passWord);
-//             }
-//         }
-//     },false);
-//     pay_con.addEventListener('touchend',function(e) {
-//         var e = e || window.event;
-//         if (e.keyCode == 8) {
-//             if (activeIndex > 0) {
-//                 activeIndex--;
-//                 reback(activeIndex);
-//                 passWord.pop();
-//             }
-//         }
-//     },false);
-//     function toNext(index) {
-//         var key = index + 1;
-//         $("#pay_input_" + key).focus();
-//     }
-//     function reback(index) {
-//         var key = index + 1;
-//         $("#pay_input_" + key).val('');
-//         $("#pay_input_" + key).focus();
-//     }
-//
-// };
-$().ready(function(){
-    var aa=$("input[type='password']");
-    var number=0;
-    var arr=[];
-    for(var i = 0; i<aa.length;i++){
-        var t = aa[i];
-        t.index = i;
-        t.setAttribute("readonly", true);
-        t.onkeyup=function(){
-            // $('.line div').addClass('color');
-            // $('.line div').each(function(index,val){
-            //     if(aa[index].value.length==0){
-            //         $('.line div').eq(index).removeClass('color');
-            //     }
-            // })
-            // if($('.color').length==6){
-            //     $('.cover').removeClass('active');
-            //     $('.return')[0].addEventListener('touchstart',function(){
-            //         location.href='main.html';
-            //     })
-            // }
-            this.value=this.value.replace(/^(.).*$/,'$1');
-            var next = this.index + 1;
-            if(next > aa.length - 1) return;
-            aa[next].removeAttribute("readonly");
-            aa[next].focus();
-=======
 $(function () {
-    var pay_con = document.querySelector('.pay_con');
-    var activeIndex = 0;
-    var passWord = [];
-    toNext(0);
-    pay_con.addEventListener('keypress',function(e) {
-        if (e.target.className.toLowerCase() == 'pay_input') {
-            e.preventDefault();
-            if (activeIndex < 6 && e.keyCode != 8) {
-                var k = String.fromCharCode(e.charCode);
-                if (/\d/.test(k)) {
-                    $("#"+e.target.id).val(1);
-                    passWord.push(k);
-                    if (activeIndex != 6 - 1) {
-                        toNext(activeIndex + 1);
-                    }
-                    activeIndex++;
-                } else {
+    // var pay_con = document.querySelector('.pay_con');
+    // var activeIndex = 0;
+    // var passWord = [];
+    // toNext(0);
+    // pay_con.addEventListener('touchstart',function(e) {
+    //     if (e.target.className.toLowerCase() == 'pay_input') {
+    //         e.preventDefault();
+    //         if (activeIndex < 6 && e.keyCode != 8) {
+    //             var k = String.fromCharCode(e.charCode);
+    //             if (/\d/.test(k)) {
+    //                 $("#"+e.target.id).val(1);
+    //                 passWord.push(k);
+    //                 if (activeIndex != 6 - 1) {
+    //                     toNext(activeIndex + 1);
+    //                 }
+    //                 activeIndex++;
+    //             } else {
+    //             }
+    //         }
+    //         if (activeIndex == 6) {
+    //             if(passWord[5].value!=""){
+    //                 // console.log(document.querySelector(".button"))
+    //                 document.querySelector(".button").href="cy-red.html"
+    //             }
+    //
+    //         }
+    //     }
+    // },false);
+    // pay_con.addEventListener('change',function(e) {
+    //     var e = e || window.event;
+    //     if (e.keyCode == 8) {
+    //         if (activeIndex > 0) {
+    //             activeIndex--;
+    //             reback(activeIndex);
+    //             passWord.pop();
+    //         }
+    //     }
+    // },false);
+    // function toNext(index) {
+    //     var key = index + 1;
+    //     $("#pay_input_" + key).focus();
+    // }
+    // function reback(index) {
+    //     var key = index + 1;
+    //     $("#pay_input_" + key).val('');
+    //     $("#pay_input_" + key).focus();
+    // }
+    //
+    // var aa=$("input[type='password']");
+    // aa.each(function (index,val) {
+    //     val.setAttribute("readonly", "readonly");
+    //
+    // })
+    // aa[0].removeAttribute("readonly");
+
+    $(".ipt-real-nick").on("input", function() {
+        //console.log($(this).val());
+        var $input = $(".ipt-fake-box input");
+        if(!$(this).val()){//无值光标顶置
+            $('.ipt-active-nick').css('left',$input.eq(0).offset().left-parseInt($('.ipt-box-nick').parent().css('padding-left'))+'px');
+        }
+        if(/^[0-9]*$/g.test($(this).val())){//有值只能是数字
+            //console.log($(this).val());
+            var pwd = $(this).val().trim();
+            for (var i = 0, len = pwd.length; i < len; i++) {
+                $input.eq(i).val(pwd[i]);
+                if($input.eq(i).next().length){//模拟光标，先将图片容器定位，控制left值而已
+                    $('.ipt-active-nick').css('left',$input.eq(i+1).offset().left-parseInt($('.ipt-box-nick').parent().css('padding-left'))+'px');
                 }
             }
-            if (activeIndex == 6) {
-                if(passWord[5].value!=""){
-                    // console.log(document.querySelector(".button"))
-                    document.querySelector(".button").href="cy-red.html"
+            $input.each(function() {//将有值的当前input后的所有input清空
+                var index = $(this).index();
+                if (index >= len) {
+                    $(this).val("");
                 }
+            });
+            if (len == 6) {
+                //执行其他操作
+                // console.log('输入完整，执行操作');
+                var button=document.querySelector(".button")
+                button.href="cy-red.html"
 
             }
->>>>>>> e71c898eb20ffc0d1e72ab33d6a712c4426348c2
+        }else{//清除val中的非数字，返回纯number的value
+            var arr=$(this).val().match(/\d/g);
+            $(this).val($(this).val().slice(0,$(this).val().lastIndexOf(arr[arr.length-1])+1));
+            //console.log($(this).val());
         }
-    },false);
-    pay_con.addEventListener('keyup',function(e) {
-        var e = e || window.event;
-        if (e.keyCode == 8) {
-            if (activeIndex > 0) {
-                activeIndex--;
-                reback(activeIndex);
-                passWord.pop();
-            }
-        }
-    },false);
-    function toNext(index) {
-        var key = index + 1;
-        $("#pay_input_" + key).focus();
-    }
-    function reback(index) {
-        var key = index + 1;
-        $("#pay_input_" + key).val('');
-        $("#pay_input_" + key).focus();
-    }
-
-    var aa=$("input[type='password']");
-    aa.each(function (index,val) {
-        val.setAttribute("readonly", "readonly");
-
-    })
-    aa[0].removeAttribute("readonly");
-
-
-
+    });
 })
-
-
-// $().ready(function(){
-//     var aa=$("input[type='password']");
-//
-//     for(var i = 0; i<aa.length;i++){
-//         var t = aa[i];
-//         t.index = i;
-//         t.setAttribute("readonly", true);
-//         t.onkeyup=function(){
-//             this.value=this.value.replace(/^(.).*$/,'$1');
-//             var next = this.index + 1;
-//             if(next > aa.length - 1) return;
-//             aa[next].removeAttribute("readonly");
-//             aa[next].focus();
-//         }
-//     }
-//     aa[0].removeAttribute("readonly");
-// })
